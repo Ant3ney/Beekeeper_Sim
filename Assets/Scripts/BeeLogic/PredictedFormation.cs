97fa44ea53a0ce3deed39cf1b831e86cea43c277
Timothy Dale Lim <limtimothy.dale@gmail.com>
2025-10-12 14:24:35 -0700

    some fixes

--- Assets/Scripts/BeeLogic/PredictedFormation.cs
+++ Assets/Scripts/BeeLogic/PredictedFormation.cs
@@ -9,6 +9,7 @@ public class PredictedFormation : MonoBehaviour
     private Spline mSpline;
     private LineRenderer mLineRenderer;
     public float lengthPerLine = 0.05f;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
